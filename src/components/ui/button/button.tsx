9e847a73e32e57f29ef2d66ab8bf8203af6e049c
Na3ar-17 <gavruk2017@gmail.com>
2025-02-22 12:33:42 +0100

    feat: updated button styles, added new color variables, and improved menu view layout

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -5,20 +5,17 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'inline-flex items-center text-zinc-100 cursor-pointer justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        default:
-          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+        default: 'bg-primary-500 shadow hover:bg-primary-600/90 max-w-fit',
+        destructive: 'bg-destructive shadow-sm hover:bg-destructive/90',
+        outline: 'border border-input bg-background shadow-sm hover:bg-accent',
         secondary:
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        ghost: 'hover:bg-accent',
+        link: 'underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-9 px-4 py-2',
--- src/views/menu-view.tsx
+++ src/views/menu-view.tsx
@@ -7,17 +7,21 @@ interface MenuViewProps {
 
 const MenuView: FC<MenuViewProps> = ({ onStartSetup }) => {
   return (
-    <div className="flex flex-col">
-      <h1 className="text-4xl text-center font-bold uppercase text-yellow-400">
-        the classic naval combat game
+    <div className="flex flex-col items-center">
+      <h1 className="text-3xl text-center uppercase font-light text-yellow-400">
+        the classic combat game
       </h1>
-      <span className="text-xs max-w-md mt-1 mb-4 text-gray-500">
-        Welcome to the game Sea Battle! The game was developed as a technical
-        task specifically for Sergey Babich's YouTube channel. To start the
-        game, click "Start Game" and enjoy!
+      <h1 className="text-zinc-100 text-8xl mt-5 text-center font-bold uppercase">
+        Battleship
+      </h1>
+      <span className="text-sm max-w-5xl text-center mx-auto mt-4 mb-10 text-zinc-100">
+        Welcome to the game Sea Battle! To start the game, click "Start Game"
+        and enjoy!
       </span>
 
-      <Button onClick={onStartSetup}>Start new game</Button>
+      <Button size={'lg'} onClick={onStartSetup}>
+        Start game
+      </Button>
     </div>
   );
 };
